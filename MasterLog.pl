342dc96cd28d9c3e319fee29e0a2b7f6de6be756
Anis <Benyoub.anis@gmail.com>
2012-10-09 11:42:06 +0200

    Fixing fu*king AI

--- MasterLog.pl
+++ MasterLog.pl
@@ -97,22 +97,28 @@ play(Y):-read(X),write(X),startGame(X,Y).
 extract(X, [X|L], L).
 extract(X, [T|R], [T|L] ):-extract(X, R, L).
 
-newList(Li,C,[],Lp,[],[],Li,K,K,C).
+newList(Li,C,[],Lp,[],[],Li,K,K2,C):- inv(K,K2).
 newList(Li,C,[K|Ls],[D|Lp],[f|X],[I|Y],NLi,NLp,NLpf,NC):-extract(I,Li,Li2),newList(Li2,C,Ls,Lp,X,Y,NLi,[D|NLp],NLpf,NC).
+newList(Li,C,[K|Ls],[D|Lp],[f|X],[I|Y],NLi,NLp,NLpf,NC):-newList(Li2,C,Ls,Lp,X,Y,NLi,[D|NLp],NLpf,NC).
+
 
 newList(Li,C,[I|Ls],[D|Lp],[v|X],[I|Y],NLi,NLp,NLpf,NC):-extract(I,Li,Li2),newList(Li2,[I|C],Ls,Lp,X,Y,NLi,[D|NLp],NLpf,NC).
+newList(Li,C,[I|Ls],[D|Lp],[v|X],[I|Y],NLi,NLp,NLpf,NC):-newList(Li2,[I|C],Ls,Lp,X,Y,NLi,[D|NLp],NLpf,NC).
 
 newList(Li,C,[K|Ls],[D|Lp],[p|X],[I|Y],NLi,NLp,NLpf,NC):-extract(I,Li,Li2),newList(Li2,[I|C],Ls,Lp,X,Y,NLi,[[I|D]|NLp],NLpf,NC).
+newList(Li,C,[K|Ls],[D|Lp],[p|X],[I|Y],NLi,NLp,NLpf,NC):-newList(Li2,[I|C],Ls,Lp,X,Y,NLi,[[I|D]|NLp],NLpf,NC).
 
-propose(4,_,_,_,_,S,Sk):-inv(S,Sk).
-propose(Size,[],Lp,C,S,Sp):- propose(Size,C,Lp,[],S,Sp).
-propose(Size,[D|Li],[O|Lp],C,S,Sp):-D \= [], Size1 is Size+1,not(member(D,O)),propose(Size1,Li,Lp,C,[D|S],Sp).
-propose(Size,[D|Li],Lp,C,S,Sp):-D \= [], propose(Size,Li,Lp,[D|C],S,Sp).
+propose(4,NLI,_,NC,S,Sk,NC,NLI):-inv(S,Sk).
+propose(Size,[],Lp,C,S,Sp,NC,NLI):- propose(Size,C,Lp,[],S,Sp,NC,NLI).
+propose(Size,[D|Li],[O|Lp],C,S,Sp,NC,NLI):-D \= [], Size1 is Size+1,not(member(D,O)),propose(Size1,Li,Lp,C,[D|S],Sp,NC,NLI).
+propose(Size,[D|Li],Lp,C,S,Sp,NC,NLI):-D \= [], propose(Size,Li,Lp,[D|C],S,Sp,NC,NLI).
 
+	
 
+solve(S,Li,Sf,Ls,Lp,C,[v,v,v,v]):-write(y).
+victory([v,v,v,v]):-write(bla).
 
-
-solve(S,Li,Sf,Ls,Lp,C):-propose(0,Li,Lp,C,S,Sp), testComb(S,Sp,R),newtList(Li,C,Ls,Lp,X,Y,NLi,Nlp,Nlpf,NC),solve(S,NLi,Sf,Ls,Nlpf,NC).
-machine(S,Li,Sf):-solve(S,Li,Sf,[X,Y,Z,D],[[],[],[],[]],[]).
+solve(S,Li,Sf,Ls,Lp,C,AR):-AR \= [v,v,v,v],propose(0,Li,Lp,C,[],Sp,KC,KLI),write( S ),write( Sp ), testComb(Sp,S,R),write( R ),newList(Li,C,Ls,Lp,R,Sp,NLi,[],Nlpf,NC),solve(S,NLi,Sf,Ls,Nlpf,NC,R).
+machine(S,Li,Sf):-solve(S,Li,Sf,[X,Y,Z,D],[[],[],[],[]],[],[]).
 
 %newList([1,2,3,4,5,6,7,8],C,[X,Y,Z,D],[[],[],[],[]],[p,v,f,f],[1,2,7,6],J,[],Q,P).
